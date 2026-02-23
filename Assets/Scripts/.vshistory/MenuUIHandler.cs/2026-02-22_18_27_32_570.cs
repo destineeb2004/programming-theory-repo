10714322d1c93fbd88dba32cd13942829a054144
Destinee Bennett <144924383+destineeb2004@users.noreply.github.com>
2026-02-22 18:32:33 -0600

    created title screen
    as said

---
+++ Assets/Scripts/.vshistory/MenuUIHandler.cs/2026-02-22_18_27_32_570.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public Button startButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/.vshistory/MenuUIHandler.cs/2026-02-22_18_29_16_722.cs
+++ Assets/Scripts/.vshistory/MenuUIHandler.cs/2026-02-22_18_29_16_722.cs
@@ -6,6 +6,18 @@ public class MenuUIHandler : MonoBehaviour
 {
     public Button startButton;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
